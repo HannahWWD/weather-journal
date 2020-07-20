38017e715dce671090adaa351e8d6580aa174d29
Hannah Ng <hannahng@hannahs-mbp-2.lan>
2020-07-20 11:14:27 -0700

    setup basic website

--- server.js
+++ server.js
@@ -1,19 +1,61 @@
-// Setup empty JS object to act as endpoint for all routes
-projectData = {};
+// express to run server and routes
+projectData = {}
 
-// Require Express to run server and routes
+const express = require('express');
 
-// Start up an instance of app
+const connectLiveReload = require('connect-livereload');
+  
 
-/* Middleware*/
-//Here we are configuring express to use body-parser as middle-ware.
+// start up an instance of app
+const app = express();
+
+app.use(connectLiveReload());
+// dependencies
+const bodyParser = require('body-parser');
+
+// middleware
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+const cors = require('cors');
+app.use(cors());
 
-// Cors for cross origin allowance
+// this is to connect the browser to the website folder
+app.use(express.static('website'))
 
-// Initialize the main project folder
-app.use(express.static('website'));
+const port = 3000;
+const server = app.listen(port, listening);
 
 
-// Setup Server
+// since it is node.js, you have to write with plain js format. or you can also "convert to es6"
+function listening() {
+    console.log('server running');
+    console.log(`running on localhost:${port}`)
+}
+
+const livereload = require('livereload');
+const lserver = livereload.createServer();
+lserver.watch('website');
+
+lserver.server.once("connection", () => {
+    setTimeout(() => {
+      lserver.refresh("/");
+    }, 100);
+  });
+
+
+
+
+// const data = []
+
+// app.post('/animal', addAnimal);
+
+// function addAnimal (req,res){
+//     data.push(req.body);
+//     console.log(data);
+//     res.send(data);
+// };
+
+// app.get("/test",function(req,res){
+//     res.send(data);
+// }) 
